cacb2f8115581171c3289bfacdab0c3f6070aaee
verylowely <svetlanamich2008@gmail.com>
2023-05-26 17:55:31 +0300

    uwu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
+<p1>Я далбаеб</p1>
